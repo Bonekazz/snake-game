f78abdbcbbcd6133bcea894e960e5f90d298a90c
Bonekazz <juracirdantas@gmail.com>
2024-01-25 12:56:15 -0300

    yaba

--- GameEngine.js
+++ GameEngine.js
@@ -1,15 +1,31 @@
 class GameEngine {
     constructor() {
-        if (GameEngine.instance === null) {
-
+        if (GameEngine.instance) {
+            throw new Error("New instance cannot be created")
         }
+        GameEngine.instance = this;
 
-        return GameEngine.instance;
+        this.window = null;
+        this.dom = null;
+        this.board = null
     }
 
     start() {
         console.log("engine started");
     }
+
+    init() {
+        this.window.addEventListener("click", () => {
+            console.log("player clicked")
+        })
+    }
+
+    setConfig(config) {
+        this.window = config.window;
+        this.dom = config.document;
+        this.board = config.BOARD;
+    }
+
 }
 
 const ENGINE = new GameEngine();
--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-import ENGINE from "./GameEngine.js";
-import Snake from "./Snake.js";
-import BOARD from "./Board.js";
+import ENGINE from './GameEngine.js';
+//import Snake from './Snake.js';
+import BOARD from './Board.js';
 
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 
 BOARD.setContext(ctx);
-BOARD.setBGColor("#e060a6")
+BOARD.setBGColor('#e060a6');
 
-ENGINE.start()
+ENGINE.setConfig({ window, document, BOARD });
+ENGINE.init();
+ENGINE.start();
